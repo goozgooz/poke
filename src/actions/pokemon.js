f716f42dc1718003dd492cb5a3715a666dbc7cc8
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-03-27 17:41:27 +0000

    basic api request working

--- src/actions/pokemon.js
+++ src/actions/pokemon.js
@@ -1,12 +1,16 @@
 import superagent from 'superagent';
 import * as pokemon from '../actiontypes/pokemon';
-
+const API_URL = process.env.REACT_APP_POKE_API_URL;
+console.log(API_URL)
 export const fetchAll = () => (store) => {
-  return superagent.get(`${process.env.POKE_API_URL}/pokemon/1/`)
-    .then(results => {
-      console.log(results);
+  return superagent.get(`${API_URL}pokemon/1`)
+    .then(res => {
+      console.log(res.body);
     })
-    .catch(console.log)
+    .catch(err => {
+      console.log('ERROR');
+      console.log(err);
+    })
 };
 
 const fetch = (pokemon) => {
