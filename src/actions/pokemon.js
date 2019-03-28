5715e5ab2ca23148074ad976e52e0f5c3df55cbc
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-03-28 16:10:25 +0000

    cleaned up console logs

--- src/actions/pokemon.js
+++ src/actions/pokemon.js
@@ -1,20 +1,11 @@
-// import Pokedex from 'pokeapi-js-wrapper';
-// const P = new Pokedex.Pokedex();
-
 import superagent from 'superagent';
 import * as Pokemon from '../actiontypes/pokemon';
 
 const API_URL = process.env.REACT_APP_POKE_API_URL;
-console.log(API_URL)
-// const list = {
-//   limit: 151,
-//   offset: 0
-// };
 
 export const fetchAll = () => (dispatch) => {
   return superagent.get(`${API_URL}pokemon/?limit=151`)
     .then(res => {
-      console.log(res)
       return dispatch(fetch(res.body.results));
     })
     .catch(err => {
